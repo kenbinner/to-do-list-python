6803674c9df35de5d3d3aa38b4daddc89872eee0
kenbinner <kengo@binner.org>
2022-12-10 14:56:50 +0000

    Functions added

--- todoapp.py
+++ todoapp.py
@@ -1,23 +1,39 @@
 list = ['Clean my room', 'Buy Groceries']
 
+# Print out To-do list for user
 def printToDo():
     print('To do:')
     print('------')
     for item in list:
         print(item)
+    print('------')
 
-option = input('Add: press 1, Remove: press 2' )
-
-if option == "1":
-    addItem = input('Add new todo... :' )
+# Option dialog
+def optionDialog():
+    option = input('Add: press 1, Remove: press 2, Exit: 3 : ' )
+    if option == "1":
+        addItem()
+    elif option == "3":
+        exit
+    else:
+        print('Input something else')
+        optionDialog()
+        
+# Adding a new to do
+def addItem():
+    addItem = input('Add new todo... : ' )
     list.append(addItem)
     print('Added!')
+    print('')
     printToDo()
-else:
-    exit
 
-# elif option == "2":
-#     removeItem = input('Remove item id... :')
-#     list.remove(list[removeItem])
+# Removing a to do
+def removeItem():
+    removeItem = input('Remove item id... :')
+    list.remove(list[removeItem])
+    print('Removed!')
+    print('')
+    printToDo()
 
-
+printToDo()
+optionDialog()
